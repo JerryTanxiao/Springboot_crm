8b16e5ffbdaa84fa0776e2a7008a9b2465443b0d
JerryTan <JerryTan_xiao@163.com>
2019-04-01 11:12:18 +0800

    a

--- src/main/java/com/SpringbootCrmApplication.java
+++ src/main/java/com/SpringbootCrmApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringbootCrmApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringbootCrmApplication.class, args);
     }
-
 }
