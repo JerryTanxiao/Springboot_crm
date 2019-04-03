089c08110446994e0170e11ed736842495ac64f1
Jack <18113515095@163.com>
2019-04-03 11:15:12 +0800

    对工作进度进行处理

--- src/main/java/com/controller/WorkPlan.java
+++ src/main/java/com/controller/WorkPlan.java
@@ -1,8 +1,25 @@
 package com.controller;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.service.WorkPlanService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * @author jack
  * @date 2019/4/2 15:04
  */
+@RestController
 public class WorkPlan {
+    @Autowired
+    private WorkPlanService work;
+
+    @RequestMapping("/get")
+    public IPage get() {
+        IPage<com.pojo.WorkPlan> workMessage = work.getWorkMessage();
+        return workMessage;
+
+    }
+
 }
