c7218f9d345556d71e2eab55a8209fa9687a9c14
Jack <18113515095@163.com>
2019-04-03 11:12:27 +0800

    对工作进度进行处理

---
+++ src/main/java/com/pojo/WorkPlan.java
@@ -0,0 +1,21 @@
+package com.pojo;
+
+import lombok.Data;
+
+/**
+ * @author jack
+ * @date 2019/4/2 18:21
+ * 工作进度实体类
+ */
+@Data
+public class WorkPlan {
+    private String uid;
+    private String nick;
+    private String usex;
+    private String utel;
+    private String email;
+    private String ustatus;
+    private String created;
+    private String uadd;
+
+}
