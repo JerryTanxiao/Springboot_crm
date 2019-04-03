4a7bd168913e7e2689c7edfe1a8390f5c5436b69
Jack <18113515095@163.com>
2019-04-03 11:13:05 +0800

    对工作进度进行处理

---
+++ src/main/java/com/service/impl/WorkPlanServiceImpl.java
@@ -0,0 +1,29 @@
+package com.service.impl;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.dao.WorkPlanMapper;
+import com.pojo.WorkPlan;
+import com.service.WorkPlanService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+/**
+ * @author jack
+ * @date 2019/4/2 18:43
+ */
+@Service
+public class WorkPlanServiceImpl extends ServiceImpl<WorkPlanMapper, WorkPlan> implements WorkPlanService {
+    private Logger logger = LoggerFactory.getLogger(WorkPlanServiceImpl.class);
+    @Override
+    public IPage<WorkPlan> getWorkMessage() {
+        Page<WorkPlan> page = new Page<WorkPlan>();
+        page.setCurrent(1);
+        page.setSize(2);
+        return baseMapper.getWorkMessage(page);
+    }
+}
